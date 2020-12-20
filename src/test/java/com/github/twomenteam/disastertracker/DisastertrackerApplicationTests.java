3851d4a9f645c0ba22269a3866dd7e4c42ef2ec8
Vasyl Teliman <vasniktel@gmail.com>
2020-12-20 18:53:18 +0200

    Add Spring boot project

---
+++ src/test/java/com/github/twomenteam/disastertracker/DisastertrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.twomenteam.disastertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisastertrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
