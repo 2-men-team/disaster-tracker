2da5ed99106e7adc8c078011d0927c1355ad0c01
Vasyl Teliman <vasniktel@gmail.com>
2020-12-29 15:37:12 +0200

    Resize scheduling pool

--- src/main/java/com/github/twomenteam/disastertracker/DisasterTrackerApplication.java
+++ src/main/java/com/github/twomenteam/disastertracker/DisasterTrackerApplication.java
@@ -2,7 +2,10 @@ package com.github.twomenteam.disastertracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @SpringBootApplication
 @EnableScheduling
