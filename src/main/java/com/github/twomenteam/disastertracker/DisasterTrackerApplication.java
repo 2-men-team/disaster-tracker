59ed5749da075f3ccb40531db20fc9e9792274c8
Vasyl Teliman <vasniktel@gmail.com>
2020-12-29 23:20:08 +0200

    Remove block hound

--- src/main/java/com/github/twomenteam/disastertracker/DisasterTrackerApplication.java
+++ src/main/java/com/github/twomenteam/disastertracker/DisasterTrackerApplication.java
@@ -2,19 +2,14 @@ package com.github.twomenteam.disastertracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-import reactor.blockhound.BlockHound;
 import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 @EnableScheduling
 public class DisasterTrackerApplication {
   public static void main(String[] args) {
-    BlockHound.install();
     ReactorDebugAgent.init();
     ReactorDebugAgent.processExistingClasses();
     SpringApplication.run(DisasterTrackerApplication.class, args);
