5ac55be03307d11deb02b333cc6c81c1d11137ba
Vasyl Teliman <vasniktel@gmail.com>
2020-12-29 23:05:08 +0200

    Add debug agent

--- src/main/java/com/github/twomenteam/disastertracker/DisasterTrackerApplication.java
+++ src/main/java/com/github/twomenteam/disastertracker/DisasterTrackerApplication.java
@@ -7,10 +7,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import reactor.blockhound.BlockHound;
+import reactor.tools.agent.ReactorDebugAgent;
+
 @SpringBootApplication
 @EnableScheduling
 public class DisasterTrackerApplication {
   public static void main(String[] args) {
+    BlockHound.install();
+    ReactorDebugAgent.init();
+    ReactorDebugAgent.processExistingClasses();
     SpringApplication.run(DisasterTrackerApplication.class, args);
   }
 }
--- src/main/java/com/github/twomenteam/disastertracker/controller/AuthController.java
+++ src/main/java/com/github/twomenteam/disastertracker/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.github.twomenteam.disastertracker.service.GoogleApiService;
 import com.google.api.services.calendar.CalendarScopes;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
